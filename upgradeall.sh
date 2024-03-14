933129dce4b907d872ea36b340c8fe43e687ced3
defipirate <defipirate123@gmail.com>
2024-03-14 12:56:22 -0500

    one click upgrade all nodes

--- upgradeall.sh
+++ upgradeall.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 
+# Replace 'file_with_ips.txt' with the actual path to your file containing IP addresses
 input_file="nodes"
 
 # Read each line (IP address) from the file
@@ -6,15 +8,20 @@ while IFS= read -r ip_address; do
     echo "Executing commands on $ip_address..."
 
     # SSH into the remote machine and execute the commands
-    ssh root@$ip_address "sudo systemctl stop ceremonyclient.service"
-    ssh root@$ip_address "cd ~/ceremonyclient && git fetch origin && git merge origin"
-    ssh root@$ip_address "cd ~/ceremonyclient/node && GOEXPERIMENT=arenas go clean -v -n -a ./..."
-    ssh root@$ip_address "cd ~/ceremonyclient/node && rm /root/go/bin/node"
-    ssh root@$ip_address "cd ~/ceremonyclient/node && ls /root/go/bin"
-    ssh root@$ip_address "cd ~/ceremonyclient/node && sleep 5 && GOEXPERIMENT=arenas go install ./..."
-    ssh root@$ip_address "cd ~/ceremonyclient/node && sleep 5 && ls /root/go/bin"
-    ssh root@$ip_address "sudo systemctl start ceremonyclient.service"
-    ssh root@$ip_address "sudo systemctl status ceremonyclient.service"
+    ssh user@$ip_address "sudo systemctl stop ceremonyclient.service"
+    ssh user@$ip_address "cd ~/ceremonyclient && git fetch origin && git merge origin"
+
+    # Determine the path to the go binary dynamically
+    go_path=$(ssh user@$ip_address "which go")
+    if [[ -n "$go_path" ]]; then
+        ssh user@$ip_address "cd ~/ceremonyclient/node && GOEXPERIMENT=arenas $go_path clean -v -n -a ./..."
+        ssh user@$ip_address "cd ~/ceremonyclient/node && $go_path install ./..."
+    else
+        echo "Error: go binary not found on $ip_address."
+    fi
+
+    ssh user@$ip_address "sudo systemctl start ceremonyclient.service"
+    ssh user@$ip_address "sudo systemctl status ceremonyclient.service"
 
     echo "Commands executed on $ip_address."
 done < "$input_file"
