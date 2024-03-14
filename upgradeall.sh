c5f082f1e32fc222c323f1b8634810e7eacf0bdb
defipirate <defipirate123@gmail.com>
2024-03-14 12:59:45 -0500

    fixed user as root

--- upgradeall.sh
+++ upgradeall.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Replace 'file_with_ips.txt' with the actual path to your file containing IP addresses
-input_file="nodes"
+input_file="nodes1"
 
 # Read each line (IP address) from the file
 while IFS= read -r ip_address; do
     echo "Executing commands on $ip_address..."
 
     # SSH into the remote machine and execute the commands
-    ssh user@$ip_address "sudo systemctl stop ceremonyclient.service"
-    ssh user@$ip_address "cd ~/ceremonyclient && git fetch origin && git merge origin"
+    ssh root@$ip_address "sudo systemctl stop ceremonyclient.service"
+    ssh root@$ip_address "cd ~/ceremonyclient && git fetch origin && git merge origin"
 
     # Determine the path to the go binary dynamically
-    go_path=$(ssh user@$ip_address "which go")
+    go_path=$(ssh root@$ip_address "which go")
     if [[ -n "$go_path" ]]; then
-        ssh user@$ip_address "cd ~/ceremonyclient/node && GOEXPERIMENT=arenas $go_path clean -v -n -a ./..."
-        ssh user@$ip_address "cd ~/ceremonyclient/node && $go_path install ./..."
+        ssh root@$ip_address "cd ~/ceremonyclient/node && GOEXPERIMENT=arenas $go_path clean -v -n -a ./..."
+        ssh root@$ip_address "cd ~/ceremonyclient/node && $go_path install ./..."
     else
         echo "Error: go binary not found on $ip_address."
     fi
 
-    ssh user@$ip_address "sudo systemctl start ceremonyclient.service"
-    ssh user@$ip_address "sudo systemctl status ceremonyclient.service"
+    ssh root@$ip_address "sudo systemctl start ceremonyclient.service"
+    ssh root@$ip_address "sudo systemctl status ceremonyclient.service"
 
     echo "Commands executed on $ip_address."
 done < "$input_file"
